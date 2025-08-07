b0cd89b8bcd32cc069e567b169a5a11546a2603e
tonmoyc <17.tonmoy@gmail.com>
2025-08-07 23:01:37 +0100

    Implement book data loading for test profile and add configuration file

--- src/main/java/com/polarbookshop/catalogservice/BookDataLoader.java
+++ src/main/java/com/polarbookshop/catalogservice/BookDataLoader.java
@@ -1,5 +1,7 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.domain.Book;
+import com.polarbookshop.catalogservice.domain.BookService;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
@@ -9,9 +11,25 @@ import org.springframework.stereotype.Component;
 @Profile("testdata")
 public class BookDataLoader {
 
+    private final BookService bookService;
+
+    public BookDataLoader(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @EventListener(ApplicationReadyEvent.class)
     public void loadBookTestData() {
         System.out.println("Loading test data for books...");
+
+        var book1 = new Book("1234567890", "Northern Lights", "Lyra Silverstar", 9.90);
+        var book2 = new Book("1234567891", "Polar Journey", "Iorek Byrnison", 12.90);
+
+        bookService.addBookToCatalog(book1);
+        bookService.addBookToCatalog(book2);
+
+        System.out.println("Test data loading completed.");
     }
+
+
 
 }
